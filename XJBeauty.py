ad11343062758e8fa803353c1efc2636635a10f3
Paweł <pfkrzyz@gmail.com>
2020-02-12 15:04:00 +0100

    Add multithread processing feature.

--- XJBeauty.py
+++ XJBeauty.py
@@ -1,9 +1,14 @@
 from Utils import init_path, init_logger
 from Utils import is_xmllint_installed, is_jq_installed, isLinux
 from Utils import is_json_file, is_xml_file
+from Utils import TimeElapsedCounter
 import os
 import subprocess
 import argparse
+import pathos.pools as pp
+import cProfile
+import pstats
+
 
 LOG_LEVEL = 'DEBUG'
 LOG_LEVEL = 'INFO'
@@ -29,14 +34,19 @@ class XJBeauty():
 
     def run(self):
         if self.test():
-            for file in os.listdir(self._input_path):
-                self._log.info('Current processing file: %s' % file)
-                if is_json_file(file):
-                    self._beauty_JSON(file)
-                elif is_xml_file(file):
-                    self._beauty_XML(file)
-                else:
-                    self._log.warn('File %s is not xml or json.' % file)
+            file_list = os.listdir(self._input_path)
+            process_pool = pp.ProcessPool(4)
+            process_pool.map(self._beauty_worker, file_list)
+            # map(self._beauty_worker, file_list)
+
+    def _beauty_worker(self, filename):
+        self._log.info('Current processing file: %s' % filename)
+        if is_json_file(filename):
+            self._beauty_JSON(filename)
+        elif is_xml_file(filename):
+            self._beauty_XML(filename)
+        else:
+            self._log.warn('File %s is not xml or json.' % filename)
 
     def _beauty_XML(self, filename):
         self._log.debug('XML beauti %s' % filename)
@@ -88,12 +98,26 @@ if __name__ == '__main__':
     parser.add_argument('--mode', choices=['xml', 'json', 'both', 'xmljson', 'jsonxml'], default='both')
     parser.add_argument('--input_folder' ,default='./input/')
     parser.add_argument('--output_folder', default='./output/')
-    parser.add_argument('--log_level',choices=['DEBUG', 'INFO'], default='INFO')
+    parser.add_argument('--log_level', choices=['DEBUG', 'INFO'], default='INFO')
+    parser.add_argument('--multithread', choices=['True', 'False'], default='False')
     arguments = parser.parse_args()
 
     LOG_LEVEL = arguments.log_level
 
-    xj_beauty = XJBeauty(arguments.mode, input_path=arguments.input_folder, output_path=arguments.output_folder)
-    xj_beauty.run()
+    # pr = cProfile.Profile()
+    # pr.enable()
+
+    with TimeElapsedCounter() as tec:
+        xj_beauty = XJBeauty(arguments.mode, input_path=arguments.input_folder, output_path=arguments.output_folder)
+        xj_beauty.run()
+    xj_beauty._log.info(str(tec))
+
+
+    # pr.disable()
+    # stats = pstats.Stats(pr)
+    # stats.sort_stats('tottime')
+    # stats.print_stats(.08)
+
+
 
 
